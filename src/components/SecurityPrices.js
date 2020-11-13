f64eee302fc5ecf1a3db4de7fadfcb2988a4f9b4
Aidan McBride <aidankmcbride@gmail.com>
2020-11-13 15:18:39 -0500

    modal view styled and opens and closes

--- src/components/SecurityPrices.js
+++ src/components/SecurityPrices.js
@@ -11,18 +11,20 @@ export default function SecurityPrices(props) {
     return (
         <div className="prices-modal">
             <div className="modal-content">
-                <header>Prices</header>
-                <div className="prices-div">
-                    {prices.map(price => (
-                        <div key={price.id} className="price-row">
-                            <p>{price.date}</p>
-                            <p>{price.amount}</p>
-                            <p>Edit</p>
-                            <p>x</p>
-                        </div>
-                    ))}
+                <div className="top-modal-wrapper">
+                    <header>Prices</header>
+                    <div className="prices-div">
+                        {prices.map(price => (
+                            <div key={price.id} className="price-row">
+                                <p>{price.date}</p>
+                                <p>{price.amount}</p>
+                                <p>Edit</p>
+                                <p>x</p>
+                            </div>
+                        ))}
+                    </div>
+                    <p>+ Add</p>
                 </div>
-                <p>+ Add</p>
                 <div className="prices-close-div">
                     <p className="prices-close-btn" onClick={() => props.closeModal()}>Close</p>
                 </div>
