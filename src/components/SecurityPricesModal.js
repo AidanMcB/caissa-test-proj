2c7b7f88cd6acea33492b5d8f029eabf42042b98
Aidan McBride <aidankmcbride@gmail.com>
2020-11-18 23:13:54 -0500

    price modal edit price is fixed, wip on add price

--- src/components/SecurityPricesModal.js
+++ src/components/SecurityPricesModal.js
@@ -1,24 +1,52 @@
-import React, { useState } from 'react';
-import PriceModal from './PriceModal'
+import React, { useState, useEffect } from 'react';
 
-export default function SecurityPricesModal(props) {    
+export default function SecurityPricesModal(props) {
 
-    const [selectedPrice, setPrice] = useState({})
-    const [showAddPrice, setAddPrice] = useState(false)
-    const [showEditPrice, setEditPrice] = useState(false)
-    const closePrice = () => {
-        setAddPrice(false)
-        setEditPrice(false)
+    const [editable, setEditable] = useState(false)
+    const [clickedPrice, setClickPrice] = useState({
+        price: {},
+        style: {
+            color: ''
+        }
+    })
+    //consider adding useEffect to rerender updated prices
+    const [myPrices, setPrices] = useState(props.children.prices)
+    useEffect(() => {
+        setPrices(props.children.prices)
+    }, [props])
+
+    //updates the entire array of prices
+    const handleDateInput = (e, selectedPrice, newDate) => {
+        let priceIndex = myPrices.findIndex(price => myPrices.indexOf(price) === myPrices.indexOf(selectedPrice))
+        myPrices[priceIndex].date = newDate
+    }
+    const handleAmountInput = (e, selectedPrice, newAmount) => {
+        let priceIndex = myPrices.findIndex(price => myPrices.indexOf(price) === myPrices.indexOf(selectedPrice))
+        myPrices[priceIndex].amount = parseInt(newAmount)
+    }
+    const numbersOnly = (e) => {
+        if (e.charCode < 48 || e.charCode > 57) {
+            e.preventDefault()
+            return false;
+        }
+    }
+    //submits that array when the modal is closed 
+    const handleClose = (e) => {
+        props.editPrice(e, props.security, myPrices)
+        props.addPrice(e, props.security, myPrices)
+        props.closePricesModal(e)
     }
 
     const handleEditClick = (price) => {
-        setPrice(price)
-        setEditPrice(true)
+        setClickPrice({
+            price: price,
+            style: {
+                color: "blue"
+            }
+        })
+        setEditable(true)
     }
 
-
-    const prices = props.children.prices
-    
     if (props.show === false) {
         return null;
     }
@@ -28,31 +56,29 @@ export default function SecurityPricesModal(props) {
                 <div className="top-modal-wrapper">
                     <header>Prices</header>
                     <div className="prices-div">
-                        {prices.map((price, index) => (
+                        {myPrices.map((price, index) => (
                             <div key={index} className="price-row">
-                                <p>{price.date}</p>
-                                <p>{price.amount}</p>
+                                <p
+                                    onKeyPress={(e) => numbersOnly(e)}
+                                    contentEditable={editable && clickedPrice.price === price}
+                                    onInput={(e) => handleDateInput(e, price, e.target.innerText)}
+                                >{price.date}</p>
+                                <p
+                                    onKeyPress={(e) => numbersOnly(e)}
+                                    contentEditable={editable && clickedPrice.price === price}
+                                    onInput={(e) => handleAmountInput(e, price, e.target.innerText)}
+                                >{price.amount}</p>
                                 <button onClick={() => handleEditClick(price)}>Edit</button>
                                 <button onClick={(e) => props.deletePrice(e, props.security, price)}>x</button>
                             </div>
                         ))}
                     </div>
-                    <button onClick={() => setAddPrice(true)}>+ Add</button>
+                    <button onClick={(e) => handleClose(e)}>+ Add</button>
                 </div>
                 <div className="prices-close-div">
-                    <button className="prices-close-btn" onClick={() => props.closePricesModal()}>Close</button>
+                    <button className="prices-close-btn" onClick={(e) => handleClose(e)}>Close</button>
                 </div>
             </div>
-            <PriceModal 
-                price={selectedPrice}
-                security={props.security}
-                showAddPrice={showAddPrice}
-                showEditPrice={showEditPrice}
-                addPrice={props.addPrice}
-                editPrice={props.editPrice}
-                closePrice={closePrice}
-                >{selectedPrice}</PriceModal>
-
         </div>
     )
 }
